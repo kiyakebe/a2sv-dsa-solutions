899bfc66411c710fc5a7825d099e2b688ca92e2b
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2023-12-21 14:58:07 +0200

    Added solution for Merge Strings Alternately

---
+++ python-track/merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        a = len(word1)
+        b = len(word2)
+        if b<a:
+            shorter = b
+            remaining = word1[b:]
+        else:
+            shorter = a
+            remaining = word2[a:]
+        ans =''
+        for i in range(shorter):
+            ans += word1[i] + word2[i]
+        return ans + remaining
