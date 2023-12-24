88064b8ee1ef2ad77e646cd1142ba239c351c4c5
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2023-12-24 14:40:41 +0200

    Added solution for Find Players With Zero or One Losses

---
+++ python-track/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        loser = {}
+        for i in matches:
+            if i[1] not in loser:
+                loser[i[1]] = 0
+            loser[i[1]] += 1
+        winner = {x[0] for x in matches if x[0] not in loser}
+
+        loser_only_once = {x for x in loser if loser[x] == 1}
+        return [sorted(list(winner)), sorted(list(loser_only_once))]
