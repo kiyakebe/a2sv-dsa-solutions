6d631586160a5b5c92b45f4699af07cec9392cfa
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2023-12-26 18:39:42 +0200

    Added solution for Spiral Matrix

---
+++ python-track/spiral-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m1, m2 = 0, len(matrix)-1
+        n1 ,n2 = 0, len(matrix[0])-1
+        x = 0
+        ans = []
+        while m2>=m1 and n2>=n1:
+            if x%4 == 0:
+                for i in range(n1, n2+1):
+                    ans.append(matrix[m1][i])
+                m1 += 1
+            elif x%4 == 1:
+                for i in range(m1, m2+1):
+                    ans.append(matrix[i][n2])
+                n2 -= 1
+            elif x%4 == 2:
+                for i in range(n2, n1-1, -1):
+                    ans.append(matrix[m2][i])
+                m2 -= 1
+            else:
+                for i in range(m2, m1-1, -1):
+                    ans.append(matrix[i][n1])
+                n1 += 1
+            x += 1
+        return ans
