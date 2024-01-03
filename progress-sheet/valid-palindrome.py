1d6a96b79b70af676b5b128510a70823197d952d
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-01-03 09:23:21 +0200

    Added solution for Valid Palindrome

---
+++ progress-sheet/valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l = 0
+        r =len(s)-1
+        print(s)
+
+        while l <= r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l] == s[r]:
+                l += 1
+                r -= 1
+            else: return False
+
+        return True
