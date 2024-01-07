77ff89e7a823519614b5bcc9cdd4f019b423fb88
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-01-08 00:08:41 +0200

    Added solution for Maximum Ice Cream Bars

---
+++ progress-sheet/maximum-ice-cream-bars.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        val = 0
+        count = 0
+        for i in costs:
+            val += i
+            if val <= coins:
+                count += 1
+            else:
+                break
+        return count
