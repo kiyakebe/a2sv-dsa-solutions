a1555cd5aa2539272f5f7ab07e44e86153970211
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-01-20 12:18:26 +0300

    Added solution for Circular Array Loop

---
+++ progress-sheet/circular-array-loop.py
@@ -0,0 +1,22 @@
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n = len(nums)
+        # if n < 2:
+        #     return False
+        checked = set()
+        for i in range(n):
+            if i not in checked:
+                cycle_set = set()
+                while True:
+                    if i in cycle_set:
+                        return True
+                    checked.add(i)
+                    cycle_set.add(i)
+                    prev, i = i, (i+nums[i])%n
+                    if prev == i:
+                        break
+                    if nums[prev] >0 and nums[i]<0:
+                        break
+
+        return False
+         
