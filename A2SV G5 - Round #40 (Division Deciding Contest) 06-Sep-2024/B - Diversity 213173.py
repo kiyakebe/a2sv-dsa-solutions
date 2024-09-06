f9c85b92448b792be62b914cb3cdfbe6d6ca3058
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-09-07 02:12:33 +0300

    Created 'B - Diversity' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/B - Diversity 213173.py
@@ -0,0 +1,18 @@
+# Problem: B - Diversity - https://codeforces.com/gym/543431/problem/B
+
+from collections import Counter
+s = input()
+k = int(input())
+
+cnt = Counter(s)
+
+if len(s) < k:
+    print("impossible")
+else:
+    ln = len(cnt)
+    change = k - ln
+    if change >= 0:
+        print(change)
+    else:
+        vals = sorted(cnt.values())
+        print(0)
