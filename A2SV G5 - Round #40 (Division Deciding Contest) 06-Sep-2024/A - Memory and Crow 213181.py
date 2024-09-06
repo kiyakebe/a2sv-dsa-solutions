18591253d3e51f9949b5df0dba28b6155c825125
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-09-07 02:17:01 +0300

    Created 'A - Memory and Crow' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/A - Memory and Crow 213181.py
@@ -0,0 +1,22 @@
+# Problem: A - Memory and Crow - https://codeforces.com/gym/543431/problem/A
+
+import sys
+
+def input():
+    return sys.stdin.readline().strip()
+
+def print(*output):
+    output = ' '.join(list(map(str, output)))
+    return sys.stdout.write(output + '\n')
+
+def lsi():
+    return [int(i) for i in input().split()]
+
+n = int(input())
+
+nums = lsi()
+
+for i in range(n-1):
+    nums[i] += nums[i+1]
+
+print(*nums)
