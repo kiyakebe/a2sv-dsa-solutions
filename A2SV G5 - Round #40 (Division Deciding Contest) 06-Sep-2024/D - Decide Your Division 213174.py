16a2cb296b2ebd90b552792cad3a0da1308a1bc8
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-09-07 02:13:43 +0300

    Created 'D - Decide Your Division' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 06-Sep-2024/D - Decide Your Division 213174.py
@@ -0,0 +1,25 @@
+# Problem: D - Decide Your Division - https://codeforces.com/gym/543431/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    cnt = 0
+
+    while n % 5 == 0:
+        n //= 5
+        cnt += 3
+    
+    while n % 3 == 0:
+        n //= 3
+        cnt += 2
+        
+    while n % 2 == 0:
+        n //= 2
+        cnt += 1
+    
+    if n > 1:
+        print(-1)
+    else:
+        print(cnt)
