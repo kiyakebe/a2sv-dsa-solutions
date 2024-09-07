43b6aad4390585cf50bdeb64224dc4c34dc47fe0
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-09-07 14:32:08 +0300

    Created 'Binary Tree Postorder Traversal' by A2SV Hub

---
+++ 07-Sep-2024/Binary Tree Postorder Traversal 213210.py
@@ -0,0 +1,20 @@
+# Problem: Binary Tree Postorder Traversal - https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.ans = []
+        
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root:
+            self.postorderTraversal(root.left)
+            self.postorderTraversal(root.right)
+            self.ans.append(root.val)
+        
+
+        return self.ans
