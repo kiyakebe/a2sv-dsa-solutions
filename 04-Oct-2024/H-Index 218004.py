030786ec70f3f253b557ee59c5a9797a2c7ce222
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-10-04 16:34:51 +0300

    Created 'H-Index' by A2SV Hub

---
+++ 04-Oct-2024/H-Index 218004.py
@@ -0,0 +1,21 @@
+# Problem: H-Index - https://leetcode.com/problems/h-index/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        left = 0
+        right = len(citations) - 1
+        n = len(citations)
+        h_index = float("-inf")
+        
+        while left <= right:
+            mid = left + (right-left)//2
+
+            if citations[mid] < n-mid:
+                h_index = max(h_index, citations[mid])
+                left = mid+1
+            else:
+                h_index = max(h_index, n-mid)
+                right = mid-1
+
+        return h_index
