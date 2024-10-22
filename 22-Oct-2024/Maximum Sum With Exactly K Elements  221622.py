51a12ac95631da3ce9187538da02e724b91a73ac
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-10-22 20:00:40 +0300

    Created 'Maximum Sum With Exactly K Elements ' by A2SV Hub

---
+++ 22-Oct-2024/Maximum Sum With Exactly K Elements  221622.py
@@ -0,0 +1,7 @@
+# Problem: Maximum Sum With Exactly K Elements  - https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/description/
+
+class Solution:
+    def maximizeSum(self, nums: List[int], k: int) -> int:
+        _max = max(nums)
+
+        return int(_max*k + (k-1)*k/2)
