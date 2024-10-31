29d7e9757b7a105f799f87ea4ca8cce50df92633
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-10-31 23:34:35 +0300

    Created 'Max Increase To Keep City Skyline' by A2SV Hub

---
+++ 31-Oct-2024/Max Increase To Keep City Skyline 222718.py
@@ -0,0 +1,28 @@
+# Problem: Max Increase To Keep City Skyline - https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        row_max = [0]*n
+        col_max = [0]*m
+        
+        increment = 0
+
+        for i in range(n):
+            for j in range(m):
+                val = grid[i][j]
+                if val > row_max[i]:
+                    row_max[i] = val
+                if val > col_max[j]:
+                    col_max[j] = val
+
+        for i in range(n):
+            for j in range(m):
+                val = grid[i][j]
+                line = min(row_max[i], col_max[j])
+                if val < line:
+                    increment += line-val
+        
+        return increment
