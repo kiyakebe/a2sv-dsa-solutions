501476ea81afaa21386211f961700a9cac34b73d
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2025-02-06 23:55:13 +0300

    Created 'Three sum' by A2SV Hub

---
+++ 06-Feb-2025/Three sum 248551.py
@@ -0,0 +1,19 @@
+# Problem: Three sum - https://leetcode.com/problems/3sum/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        pairs = set()
+        n = len(nums)
+        for i in range(n):
+            l = i+1
+            r = n-1
+            while l < r:
+                if nums[i] + nums [l] + nums[r] == 0:
+                    pairs.add((nums[i], nums [l], nums[r]))
+                    l += 1
+                    r -= 1
+                elif nums[i] + nums [l] + nums[r] < 0:
+                    l+=1
+                else: r-= 1
+        return [[x, y, z] for (x, y, z) in pairs]
